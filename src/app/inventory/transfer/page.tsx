76fa066866b0c2360eb35e3a0d1651c95f25a24f
Devish Mittal <devishmittal1@gmail.com>
2025-08-22 17:51:29 +0530

    extra

--- src/app/inventory/transfer/page.tsx
+++ src/app/inventory/transfer/page.tsx
@@ -184,7 +184,7 @@ export default function InventoryTransferPage() {
             <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
             </svg>
-            + Create Transfer
+            Create Transfer
           </button>
         </div>
 
