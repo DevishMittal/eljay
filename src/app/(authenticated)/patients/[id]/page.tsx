5dfc9b38e0ab359b65327e0587aaf2d1a95899fe
Devish Mittal <devishmittal1@gmail.com>
2025-09-24 23:33:05 +0530

    remove showing of no invoice found

--- src/app/(authenticated)/patients/[id]/page.tsx
+++ src/app/(authenticated)/patients/[id]/page.tsx
@@ -1433,13 +1433,13 @@ export default function PatientProfilePage({ params }: { params: Promise<{ id: s
                         <p className="mt-2 text-gray-600 text-xs">Loading appointments...</p>
                       </div>
                     ) : appointments.length === 0 ? (
-                      <div className="text-center py-8">
-                        <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center mx-auto mb-3">
+                      <div className="text-center ">
+                        {/* <div className="w-12 h-12 bg-gray-100 rounded-full flex items-center justify-center mx-auto mb-3">
                           <svg className="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                           </svg>
-                        </div>
-                        <p className="text-gray-500 text-xs">No appointments found</p>
+                        </div> */}
+                        {/* <p className="text-gray-500 text-xs">No appointments found</p> */}
                       </div>
                     ) : (
                       <div className="space-y-3">
